15c637185af55e339523418153a80fdbf897886f
Bloodangel52 <kira.carlosbeltrandominguez@gmail.com>
2022-05-11 20:06:35 +0200

    inicio nuevo con tuplas

---
+++ setings.py
@@ -0,0 +1,16 @@
+level_map = [
+    'X                          X',
+    'X                          X',
+    'X                          X',
+    'X   XXXX                   X',
+    'X                  P       X',
+    'X                XXXX      X',
+    'X                          X',
+    'X   XXXX                   X',
+    'X                          X',
+    'XXXXXXXXXXXXXXXXXXXXXXXXXXXX']
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map) * tile_size
+
--- ventana.py
+++ ventana.py
@@ -1,13 +1,10 @@
 import pygame
 import sys
-import Personaje
 pygame.init()
 
 SIZE = (800, 500)
 
 screen = pygame.display.set_mode(SIZE)
-
-personaje = Personaje(screen.)
 
 while True:
     for event in pygame.event.get():
