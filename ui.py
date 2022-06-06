01ff2d664e896a35132b4e4689d11687797c5046
JavierGarridoSanchez <100608798+JavierGarridoSanchez@users.noreply.github.com>
2022-06-06 20:40:54 +0200

    Delete ui.py

--- ui.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class UI:
-    def __init__(self, surface):
-        # setup
-        self.display_surface = surface
-
-        # health
-        self.health_bar = pygame.image.load('graphics/ui/healyh_bar.png')
