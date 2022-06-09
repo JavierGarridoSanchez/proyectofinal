90b0167232452c96b5ad844234c62567271cd4e2
JavierGarridoSanchez <100608798+JavierGarridoSanchez@users.noreply.github.com>
2022-06-09 14:51:10 +0200

    Delete connection_db.py

--- connection_db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-from sqlite3.dbapi2 import Connection
-
-
-class ConnectionDB:
-
-    def __init__(self):
-        self.my_connection = sqlite3.connect("bd/juegonieve")
-        self.my_cursor = self.my_connection.cursor()
